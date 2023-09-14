ba6252f245a4c584864bda37e3126ca86256e496
bhaskar8074 <bhaskar19191@gmail.com>
2023-09-15 03:20:04 +0530

    add some more styles and initiate to implement Posts in application

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,7 +9,8 @@ class Ability
     #   return unless user.present?
     #   can :read, :all
     return unless user.admin?
-       can :manage, :all
+    can :read, User
+    can :destroy, User
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :friendships, dependent: :destroy
-  has_many :friends, class_name: "User", through: :friendships, source: :friend
-  has_many :sent_messages, class_name: 'Message', foreign_key: 'sender_id'
-  has_many :received_messages, class_name: 'Message', foreign_key: 'receiver_id'
+  has_many :friends, class_name: "User", dependent: :destroy, through: :friendships, source: :friend
+  has_many :sent_messages, class_name: 'Message',dependent: :destroy, foreign_key: 'sender_id'
+  has_many :received_messages, class_name: 'Message',dependent: :destroy, foreign_key: 'receiver_id'
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
